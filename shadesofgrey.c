4116e99154fba2a72fa7b96ff07d8395a229ac32
Roberto Montagna <roberto.montagna@gmail.com>
2012-03-19 16:59:57 +0000

    Added license note.

--- shadesofgrey.c
+++ shadesofgrey.c
@@ -1,3 +1,20 @@
+/* shadesofgrey.c - Shades of Grey plugin for the Gimp.
+ * Copyright (C) 2012 Roberto Montagna.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libgimp/gimp.h>
 #include <libgimp/gimpui.h>
 #include <math.h>
